e9c6aa23ca6d86f39d39c34a2be9ef30b333ef30
KitYip <chikityip11@gmail.com>
2021-12-02 01:38:59 +0800

    update

--- pages/Components/UserType.tsx
+++ pages/Components/UserType.tsx
@@ -8,10 +8,13 @@ const UserType: React.FC = function () {
     return (
     <div className={styles.UserType}>
         <div className={styles.publicUserType}>If you are HSU students, please click this button.
-            <div className={styles.UserType}><Button variant="contained" className={styles.hsuButton}> HSU </Button></div>
+            <div className={styles.UserType}><Button variant="contained"> HSU </Button></div>
         </div>
         <div className={styles.hsuUserType}>Else, please click this button.
             <div className={styles.UserType}><Button variant="contained"> Public </Button></div>
+        </div>
+        <div className={styles.backLogin}>If you want to back to login page, please click this button.
+            <div className={styles.UserType}><Button variant="contained"> Back </Button></div>
         </div>
     </div>
    )
