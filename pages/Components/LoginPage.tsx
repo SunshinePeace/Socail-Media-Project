3020327b1e71af2d33f053750dc7207017c3a37b
MyChiLaM <chilam1215@gmail.com>
2021-12-03 20:45:03 +0800

    css of loginpage

--- pages/Components/LoginPage.tsx
+++ pages/Components/LoginPage.tsx
@@ -1,11 +1,15 @@
 import { Button, Link } from '@mui/material';
 import { TextField } from '@mui/material';
+import Box from '@mui/material/Box';
 import React, { useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import styles from '../../styles/Home.module.css'
+import { signin, signup } from "./AuthContexts";
 import ErrorText from './Utils/Error';
 import logging from './Utils/logging';
-import { useAuth } from '../Components/AuthContexts'
+import { BrowserRouter as Router  } from 'react-router-dom';
+import SignUp from './SignUp';
+import { useStateValue } from './Useless/StateProvider';
+import { actionTypes } from './Useless/Reducer';
 // import { useCookies } from 'react-cookie';
 
 
@@ -14,8 +18,6 @@ import { useAuth } from '../Components/AuthContexts'
 const LoginPage: React.FC = function () {
 
 
-
-    const { login } = useAuth();
     const [authenticating, setAuthenticating] = useState<boolean>(false);
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
@@ -23,45 +25,13 @@ const LoginPage: React.FC = function () {
    // const [cookies, setCookie] = useCookies(['user']);
 
 
-    function handleSubmit() {
+
+
+
+
+
+    const TraditionalSignIn = () => {
         if (error !== '') setError('')
-
-        setAuthenticating(true);
-        login(email, password)
-            .then(result => {
-                logging.info(result);
-                localStorage.setItem('Email', JSON.stringify(email));
-
-
-
-            })
-            .catch(error => {
-                logging.error(error)
-                setAuthenticating(false)
-                setError("Unable To Sign In. Please try it later") // More Command
-
-            });
-
-
-        
-
-        
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*if (error !== '') setError('')
         
         setAuthenticating(true);
         signin(email, password)
@@ -78,50 +48,59 @@ const LoginPage: React.FC = function () {
                 setError("Unable To Sign In. Please try it later") // More Command
 
             });
+    }
 
-*/
-    
+
+
     return (
+            
+        <body className={styles.loginbody}>
 
             <div className={styles.loginpage}>
-            <div className={styles.loginpage__left}>
-                <img src="/90604070_p0.png" alt= "" />
+
+            <div className={styles.flexbox}>
+
+                    <div className={styles.png}>
+                        <img src="/90604070_p0.png" width="250" height="200" alt="" />
+                    </div>
+
+                    <div className={styles.loginpage_email}>
+                        <TextField type="email"
+                            id="email"
+                            onChange={x => setEmail(x.target.value)}
+                            value={email}
+                            placeholder="Enter Email"
+                            variant="standard" />
+                    </div>
+
+                    <div className={styles.loginpage_pw}>
+                        <TextField
+                            autoComplete="new-password"
+                            type="password"
+                            id="password"
+                            onChange={x => setPassword(x.target.value)}
+                            value={password}
+                            placeholder="Enter Password"
+                            variant="standard" />
+                    </div>
+
+                    <Button
+                        disabled={authenticating}
+                        color="secondary"
+                        onClick={() => TraditionalSignIn()}
+                        href="/"
+                    >
+                        Sign In
+                    </Button>
+                    <p>Do not have account?
+                        <Link href="../Components/SignUp"> Sign Up </Link>
+                    </p>
+                    <ErrorText error={error} />
+
             </div>
 
-            <div className={styles.loginpage__right}>
-                <p>Sign In</p>
-                <TextField type="email"
-                    id="email"
-                    onChange={x => setEmail(x.target.value)}
-                    value={email}
-                    placeholder="Enter Email"
-                    variant="standard" />
-                <TextField
-                    autoComplete="new-password"
-                    type="password"
-                    id="password"
-                    onChange={x => setPassword(x.target.value)}
-                    value={password}
-                    placeholder="Enter Password"
-                    variant="standard" />
             </div>
-
-
-
-            <Button
-                disabled={authenticating}
-                color="secondary"
-                onClick={() => handleSubmit()}
-                
-            >
-                Sign In
-            </Button>
-            <p>Do not have account?
-                <Link href="../Components/SignUp"> Sign Up </Link>
-            </p>
-            <ErrorText error={error} />
-           
-            </div>
+        </body>
 
 
 
